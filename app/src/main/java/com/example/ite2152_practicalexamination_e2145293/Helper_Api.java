430aa916093c4daee0fc9fbf7a7741ea304c2689
Mohomed Rishard <118299354+rishard1993@users.noreply.github.com>
2024-06-04 08:16:58 +0530

    Added Helper_Api class to handle API requests.

---
+++ app/src/main/java/com/example/ite2152_practicalexamination_e2145293/Helper_Api.java
@@ -0,0 +1,39 @@
+package com.example.ite2152_practicalexamination_e2145293;
+
+import android.content.Context;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public class Helper_Api {
+    private static Helper_Api instance;
+    private RequestQueue requestQueue;
+    private final Context ctx;
+
+    private Helper_Api(Context context) {
+        ctx = context;
+        requestQueue = getRequestQueue();
+
+    }
+
+    public static synchronized Helper_Api getInstance(Context context) {
+        if (instance == null) {
+            instance = new Helper_Api(context);
+        }
+        return instance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (requestQueue == null) {
+            // getApplicationContext() is key, it keeps you from leaking the
+            // Activity or BroadcastReceiver if someone passes one in.
+            requestQueue = Volley.newRequestQueue(ctx.getApplicationContext());
+        }
+        return requestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        getRequestQueue().add(req);
+    }
+}
+
--- app/src/main/java/com/example/ite2152_practicalexamination_e2145293/MainActivity.java
+++ app/src/main/java/com/example/ite2152_practicalexamination_e2145293/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.ite2152_practicalexamination_e2145293;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +11,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
 public class MainActivity extends AppCompatActivity {
 
+    TextView textView;
+    Button button;
+    String TAG = "WeatherRequest";
+    RequestQueue queue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +33,59 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+         textView = findViewById(R.id.textView);
+         button = findViewById(R.id.buttonRequest);
+
+
+        // Instantiate the RequestQueue.
+        //RequestQueue queue = Volley.newRequestQueue(this);
+        queue = Helper_Api.getInstance(this.getApplicationContext()).
+                getRequestQueue();
+
+        String url = "https://api.weatherapi.com/v1/current.json?q=7.247431377255542,80.47446904634002&key=d03b1eb170264c5e8ec10840240406";
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Request a string response from the provided URL.
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                // Display the first 500 characters of the response string.
+                                textView.setText("Response is: " + response.substring(0,500));
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        textView.setText("Error: " + error.getMessage());
+                    }
+                });
+
+                // Set the tag on the request.
+                stringRequest.setTag(TAG);
+
+                // Add the request to the RequestQueue.
+                //queue.add(stringRequest);
+
+                // Add a request (in this example, called stringRequest) to your RequestQueue.
+                Helper_Api.getInstance(getApplicationContext()).addToRequestQueue(stringRequest);
+            }
+        });
+
+
+
+
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (queue != null) {
+            queue.cancelAll(TAG);
+        }
     }
 }
